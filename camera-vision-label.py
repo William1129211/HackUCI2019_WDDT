e650ffb582588561334e01faf408b991a3b40358
Trevor Doan <trevordoan@gmail.com>
2019-02-16 15:39:00 -0800

    added functionality to take and process multiple photos

--- camera-vision-label.py
+++ camera-vision-label.py
@@ -13,18 +13,17 @@ import json
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
-def takephoto():
-    camera = picamera.PiCamera()
-    camera.capture('image.jpg')
+def takephoto(count, camera):
+    camera.capture('image' + str(count) + '.jpg')
 
-def main():
-    takephoto() # First take a picture
+def sendPhotoReceiveJSON(count, camera):
+    takephoto(count, camera) # First take a picture
     """Run a label request on a single image"""
 
     credentials = GoogleCredentials.get_application_default()
     service = discovery.build('vision', 'v1', credentials=credentials)
 
-    with open('image.jpg', 'rb') as image:
+    with open('image' + str(count) + '.jpg', 'rb') as image:
         image_content = base64.b64encode(image.read())
         service_request = service.images().annotate(body={
             'requests': [{
@@ -39,6 +38,15 @@ def main():
         })
         response = service_request.execute()
         print json.dumps(response, indent=4, sort_keys=True)	#Print it out and make it somewhat pretty.
+
+def main():
+    cntr = 1
+    camera = picamera.PiCamera()
+    while(cntr < 3):
+        sendPhotoReceiveJSON(cntr, camera)
+        cntr += 1
+
+    
 
 if __name__ == '__main__':
 
