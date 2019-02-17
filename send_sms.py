8b425b557e130988387e83cfa6f0a8a2640125ad
William Yang <williy3@uci.edu>
2019-02-16 17:03:17 -0800

    Update message program with different conditions.

--- send_sms.py
+++ send_sms.py
@@ -7,12 +7,21 @@ account_sid = 'AC920f2da359f4d8b4af7b9478a0ef7ccc'
 auth_token = 'baa0db35e954d100ef1c8ab6daacafef'
 client = Client(account_sid, auth_token)
 
-message = client.messages \
-                .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+19495369863',
-                     to='+19493003552'
-                 )
+#usr_num = input("Enter your phone number")
+usr_num = '+19493003552'
+
+def send_message(usr_num:str, condition:int):
+    if condition ==: 0:
+        body_text = "Dog is at home now, safe and happy :)"
+    elif condition == 1:
+        body_text = "A glutonous monstrousity is at your doorstep"
+    elif condition == 2;
+        body_text = "Someone is on your doorstep..."
+        
+    message = client.messages.create( \
+        body=body_text,
+        from_='+19495369863',
+        to=usr_num)
 
 print(message.sid)
 
